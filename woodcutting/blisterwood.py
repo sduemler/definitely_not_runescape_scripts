5906be9d85fea1bd2bc8af5373975f251535046e
sduemler <samduemler@gmail.com>
2023-12-31 15:18:54 -0500

    Added logging out before auto log

--- woodcutting/blisterwood.py
+++ woodcutting/blisterwood.py
@@ -2,6 +2,16 @@ import time
 import random
 import pyautogui
 
+
+#Get the logout X
+print("Place your cursor over the logout X.")
+time.sleep(3)
+xX, yX = pyautogui.position()
+
+print("Place your cursor over the logout button.")
+time.sleep(3)
+logoutX, logoutY = pyautogui.position()
+
 # Gives the user time to place the cursor where they want to continually click for woodcutting
 print("Make sure to position your mouse on the tree you want to cut!")
 time.sleep(3)
@@ -14,7 +24,9 @@ print("Starting now!")
 # This will be the position that continually clicks to cut wood
 clickX, clickY = pyautogui.position()
 
-for x in range(125):
+
+#140 runs is usually just under 6 hours to prevent auto logging
+for x in range(140):
   print('Running loop #' + str(x + 1))
   for y in range(10):
     pyautogui.moveTo(clickX + (random.random() * 10), clickY + (random.random() * 10))
@@ -31,4 +43,16 @@ for x in range(125):
       time.sleep(0.5)
       pyautogui.keyUp('shift')
   time.sleep(0.1)
+
+#Logging out
+print("Logging out now.")
+pyautogui.moveTo(xX, yX)
+time.sleep(0.3)
+pyautogui.click()
+
+pyautogui.moveTo(logoutX, logoutY)
+time.sleep(0.3)
+pyautogui.click()
+
+print("Logged out.")
 
